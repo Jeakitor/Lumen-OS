ab85c6c774f20b42d6d254efa7b3d4c0cf5a53f4
Gregory "Jeakitor" Mathew <gregorymathew1102@gmail.com>
2026-01-17 22:22:18 +0530

    Add logging setup to the installer

--- installer/core/installer.py
+++ installer/core/installer.py
@@ -1,4 +1,8 @@
+from installer.core.logger import setup_logger, log
+
 def main():
+    setup_logger()
+    log("Installer started")
     print("Lumen OS Installer starting...")
 
 if __name__ == "__main__":
