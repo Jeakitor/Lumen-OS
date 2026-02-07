e21af9a04df7be0c0c08cd9bf101da522f1664b7
Gregory "Jeakitor" Mathew <gregorymathew1102@gmail.com>
2026-02-07 12:05:03 +0530

    Integrate hardware detection into the installer
    Added hardware detection to the installer.

--- installer/core/installer.py
+++ installer/core/installer.py
@@ -1,9 +1,16 @@
 from installer.core.logger import setup_logger, log
+from installer.core.hardware import detect_hardware
 
 def main():
     setup_logger()
     log("Installer started")
     print("Lumen OS Installer starting...")
+    
+    hardware=detect_hardware()
+    print("Detected Hardware:")
+    for key, value in hardware.items():
+        print(f" {key}; {value}")
+    
 
 if __name__ == "__main__":
     main()
