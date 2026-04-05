0b3415603257ea1ca6caeac29c2c8522da87f566
Gregory "Jeakitor" Mathew <gregorymathew1102@gmail.com>
2026-04-05 14:05:34 +0530

    Add PACKAGE_MAP for software package definitions
    Added a PACKAGE_MAP dictionary to define various software packages and their sources.

--- installer/core/packages.py
+++ installer/core/packages.py
@@ -1,1 +1,22 @@
-
+PACKAGE_MAP = {
+    "browser": {
+        "package": "firefox",
+        "source": "pacman"
+    },
+    "code_editor": {
+        "package": "code",
+        "source": "pacman"
+    },
+    "media_player": {
+        "package": "vlc",
+        "source": "pacman"
+    },
+    "office_tools": {
+        "package": "libreoffice",
+        "source": "pacman"
+    },
+    "git": {
+        "package": "git",
+        "source": "pacman"
+    }
+}
