a7ca5dfc62c82c78de27bb7d0f295d153c57835a
Gregory "Jeakitor" Mathew <gregorymathew1102@gmail.com>
2026-04-09 11:34:21 +0530

    Fix formatting of package entries in packages.py

--- installer/core/packages.py
+++ installer/core/packages.py
@@ -4,18 +4,17 @@ PACKAGE_MAP = {
         "source": "pacman"
     },
     "code_editor": {
-    "package": "visual-studio-code-bin",
-    "source": "aur"
-    }
+        "package": "visual-studio-code-bin",
+        "source": "aur"
     },
     "media_player": {
         "package": "vlc",
         "source": "pacman"
     },
     "office_tools": {
-    "package": "org.libreoffice.LibreOffice",
-    "source": "flatpak"
-    }
+        "package": "org.libreoffice.LibreOffice",
+        "source": "flatpak"
+    },
     "git": {
         "package": "git",
         "source": "pacman"
